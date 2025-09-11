e9529ad2abe520808c3f663e1f74c7e704835f0b
Anirudh Saksena <anen08saks@gmail.com>
2025-09-11 13:18:20 +0530

    Done broadcasting

--- numpy/advanced_numpy/broadcasting.py
+++ numpy/advanced_numpy/broadcasting.py
@@ -9,14 +9,7 @@ The smaller array is 'broadcast' among the larger one so that they may have comp
 import numpy as np
 
 a = np.arange(6).reshape(2, 3)
-b = np.arange(3).reshape(1, 3)
-
-
-print(f"Matrix A\n{a}\n")
-print(f"Vector B\n{b}\n")
-
-
-print(f"A+B\n{a+b}\n")
+b = np.arange(3).reshape(3)
 
 """
 NumPy Broadcasting Rules:
@@ -33,4 +26,38 @@ Examples:
 - (5, 1) + (1, 4) -> (5, 4)
 - (3, ) + (3, 3) -> (3, 3)
 - (2, 3) + (2,) -> (2, 3)
+"""
+
+print(f"Matrix A\n{a}\n")
+print(f"Vector B\n{b}\n")
+
+print(f"A+B\n{a+b}\n")
+
+# Same dimensions
+
+a = np.arange(3).reshape(1, 3)
+b = np.arange(4).reshape(4, 1)
+
+print(f"Matrix A\n{a}\n")
+print(f"Matrix B\n{b}\n")
+print(f"Same dimensions A+B\n{a+b}\n")
+
+"""
+Why 1 can stretch but not 2 → 4
+
+If a dimension is 1, it means:
+
+    “I only have one value along this axis, so I can safely repeat it as many times as needed.”
+
+If a dimension is 2, it means:
+
+    “I already have two different values along this axis.”
+
+If we try to stretch 2 → 4, NumPy has no idea how to duplicate those values:
+
+    Should it repeat each element twice?
+    Or tile them in some other pattern?
+    Or interpolate new values?
+
+This would introduce ambiguity. NumPy avoids that by requiring that only 1 can be expanded.
 """
