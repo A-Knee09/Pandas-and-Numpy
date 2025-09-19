6d956577e6e3267ff6e90cb9a4c2fa09b669654f
Anirudh Saksena <anen08saks@gmail.com>
2025-09-19 22:13:22 +0530

    Did some questions that require filtering the dataframe, will do rest later

---
+++ pandas/pandas_dataframe/filtering_dataframe.py
@@ -0,0 +1,44 @@
+import pandas as pd
+
+movies = pd.read_csv("./csv/movies.csv")
+ipl = pd.read_csv("./csv/ipl-matches.csv")
+
+# print(f"{ipl.head(2)}\n")
+# print(f"{movies.head(2)}\n")
+
+# Find all the final winners
+
+"""
+The csv has a column called match number.
+From this column we extract all the matches having match number as final
+The above would return a boolean series which we can use as a mask for boolean indexing
+We then use the new table and extract the season and the winning team
+"""
+
+mask = ipl["MatchNumber"] == "Final"
+print(f"{mask}\n")
+
+finals_ipl_df = ipl[mask]
+print(f"Teams qualified for Finals\n{finals_ipl_df}\n")
+
+print("Winning Teams and their Seasons\n")
+print(f"{finals_ipl_df[['Season','WinningTeam']]}\n")
+
+# All this could be done in a single line as well : ipl[ipl['MatchNumber'] == 'Final'][['Season','WinningTeam']]
+
+# How many super over finishes have occured
+
+"""
+Basically finding out the matches where super over occured and then the number of rows will provide the answer
+"""
+
+print(
+    f"Number of matches where superover have happened: {ipl[ipl['SuperOver'] == 'Y'].shape[0]}\n"
+)
+
+# How many matches csk won in kolkata
+
+csk_wins = ipl[
+    (ipl["City"] == "Kolkata") & (ipl["WinningTeam"] == "Chennai Super Kings")
+].shape[0]
+print(f"Number of matches CSK won in Kolkata : {csk_wins}\n")
